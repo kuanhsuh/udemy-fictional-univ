eed699149ab876e713bb982312c0d4d2708a92ea
dannyhuang <kuanhsuh@gmail.com>
2018-12-04 22:38:31 +0800

    Finish 44

--- archive-event.php
+++ archive-event.php
@@ -1,23 +1,8 @@
-<?php get_header(); ?>
-<div class="page-banner">
-    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg'); ?>;);"></div>
-    <div class="page-banner__content container container--narrow">
-      <h1 class="page-banner__title">
-      All Events
-      <?php// if (is_category()) {
-//    single_cat_title();
-//}
-//      if (is_author()) {
-//          echo 'Post by ';
-//          the_author();
-//      }
-      ?>
-      </h1>
-      <div class="page-banner__intro">
-        <p>See what is going on in our world</p>
-      </div>
-    </div>
-  </div>
+<?php get_header();
+pageBanner(array(
+  'title' => 'All Events',
+  'subtitle' => 'See what is going on in our world'
+));?>
 
 <div class="container container--narrow page-section">
 <?php
