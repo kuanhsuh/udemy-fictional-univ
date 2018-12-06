8777d82fe8f19d3ac6e017b5549e805aae4f2664
dannyhuang <kuanhsuh@gmail.com>
2018-12-06 13:36:51 +0800

    Finish 47 Campus Map on Front-End

--- functions.php
+++ functions.php
@@ -16,19 +16,20 @@ function pageBanner($args = null)
         }
     } ?>
     <div class="page-banner">
-      <div class="page-banner__bg-image" style="background-image: url(<?php echo $args['photo']; ?>;);"></div>
-        <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title"><?php echo $args['title'] ?></h1>
-        <div class="page-banner__intro">
-            <p><?php echo $args['subtitle']; ?></p>
+        <div class="page-banner__bg-image" style="background-image: url(<?php echo $args['photo']; ?>;);"></div>
+            <div class="page-banner__content container container--narrow">
+            <h1 class="page-banner__title"><?php echo $args['title'] ?></h1>
+            <div class="page-banner__intro">
+                <p><?php echo $args['subtitle']; ?></p>
+            </div>
         </div>
-      </div>
     </div>
 <?php
 }
 
 function university_files()
 {
+    wp_enqueue_script('googleMap', '//maps.googleapis.com/maps/api/js?key=AIzaSyBj60OKJ0cHsaCYEyddVS2la1GPoFptKLQ', null, '1.0', true);
     wp_enqueue_script('main-university-js', get_theme_file_uri('/js/scripts-bundled.js'), null, microtime(), true);
     wp_enqueue_style('custom-google-fonts', 'https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
     wp_enqueue_style('font-awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
@@ -75,3 +76,10 @@ function university_adjust_queries($query)
 }
 
 add_action('pre_get_posts', 'university_adjust_queries');
+
+function universityMapKey($api)
+{
+    $api['key'] = 'AIzaSyBj60OKJ0cHsaCYEyddVS2la1GPoFptKLQ';
+    return $api;
+}
+add_filter('acf/fields/google_map/api', 'universityMapKey');
