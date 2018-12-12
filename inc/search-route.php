cb1b0f402db37ded11936d4e28b8a29673a216d9
dannyhuang <kuanhsuh@gmail.com>
2018-12-12 09:12:50 +0800

    64 Query and keyword searching

---
+++ inc/search-route.php
@@ -0,0 +1,28 @@
+<?php
+
+add_action('rest_api_init', 'universityRegisterSearch');
+
+function universityRegisterSearch()
+{
+    register_rest_route('university/v1', 'search', array(
+    "methods" => WP_REST_SERVER::READABLE,
+    "callback" => 'universitySearchResults'
+  ));
+}
+
+function universitySearchResults($data)
+{
+    $professors = new WP_Query(array(
+      'post_type' => 'professor',
+      's' => sanitize_text_field($data['term'])
+    ));
+    $professorsResults = array();
+    while ($professors->have_posts()) {
+        $professors->the_post();
+        array_push($professorsResults, array(
+          'title' => get_the_title(),
+          'permalink' => get_the_permalink()
+        ));
+    }
+    return $professorsResults;
+}
