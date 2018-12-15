0990549a157bd8242035ff6f3b46e9b8e204af25
dannyhuang <kuanhsuh@gmail.com>
2018-12-15 21:43:56 +0800

    75 Open registration II

--- functions.php
+++ functions.php
@@ -123,3 +123,27 @@ function noSubsAdminBar()
         show_admin_bar(false);
     }
 }
+
+
+// Customize Login Screen
+add_filter('login_headerurl', 'ourHeaderUrl');
+
+function ourHeaderUrl()
+{
+    return esc_url(site_url('/'));
+}
+
+add_action('login_enqueue_scripts', 'ourLoginCSS');
+
+function ourLoginCSS()
+{
+    wp_enqueue_style('university_main_styles', get_stylesheet_uri(), null, microtime());
+    wp_enqueue_style('custom-google-fonts', 'https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+}
+
+add_filter('login_headertitle', 'ourLoginTitle');
+
+function ourLoginTitle()
+{
+    return get_bloginfo('name');
+}
