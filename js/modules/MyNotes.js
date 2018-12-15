23d52c64bdc1f354118edff8db7ad5eb49391b02
dannyhuang <kuanhsuh@gmail.com>
2018-12-16 07:13:20 +0800

    78 My Notes Front-end

--- js/modules/MyNotes.js
+++ js/modules/MyNotes.js
@@ -11,7 +11,19 @@ class MyNotes {
   }
 
   deleteNote() {
-    alert('you clicked delete');
+    $.ajax({
+      beforeSend: (xhr) => {xhr.setRequestHeader('X-WP-Nonce', universityData.nonce)},
+      url: universityData.root_url + '/wp-json/wp/v2/note/89',
+      type: 'DELETE',
+      success: (response) => {
+        console.log('success delete')
+        console.log(response)
+      },
+      error: (err) => {
+        console.log('SORRY')
+        console.log(err)
+      }
+    });
   }
 }
 
