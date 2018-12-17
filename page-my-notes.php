edf79032192200c83d5debab08e630f375a6528f
dannyhuang <kuanhsuh@gmail.com>
2018-12-18 06:12:45 +0800

    74 Note Permissions and Security

--- page-my-notes.php
+++ page-my-notes.php
@@ -31,7 +31,7 @@ get_header();
           <input readonly class="note-title-field" type="text" value="<?php echo str_replace('私密：', '', esc_attr(get_the_title())) ?>">
           <span class="edit-note"><i class="fa fa-pencil" aria-hidden="true"></i>Edit</span>
           <span class="delete-note"><i class="fa fa-trash-o" aria-hidden="true"></i>Delete</span>
-          <textarea readonly class="note-body-field"><?php echo esc_attr(get_the_content()); ?></textarea>
+          <textarea readonly class="note-body-field"><?php echo esc_textarea(get_the_content()); ?></textarea>
           <span class="update-note btn btn--blue btn--small"><i class="fa fa-arrow-right" aria-hidden="true"></i>Save</span>
         </li>
       <?php
